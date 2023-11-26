dbfcab3759f6164f4d99473a5e084a24dc771bd4
Ramon <ramonwalther98@gmail.com>
2023-11-26 13:57:49 +0100

    Improved logging

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 import time
 import random
+import logging
 import pyautogui as pag
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s: %(message)s',
+    level=logging.DEBUG,
+    datefmt='[%Y-%m-%d %H:%M:%S]')
 
 
 def random_timeout():
@@ -18,7 +25,7 @@ def get_center_xy(box):
 if __name__ == '__main__':
     buttons_dir = "./buttons/"
     buttons_files = os.listdir(buttons_dir)
-    print(f'\nStarting to searching for buttons {buttons_files}')
+    logging.info(f'Starting to search for buttons {buttons_files}\n')
     while True:
         for button_file in buttons_files:
             button_path = os.path.join(buttons_dir, button_file)
@@ -27,15 +34,14 @@ if __name__ == '__main__':
                 buttons_gen = pag.locateAllOnScreen(button_path, grayscale=True, confidence=0.9)
                 buttons = [get_center_xy(x) for x in buttons_gen]
                 if len(buttons) >= 1:
-                    print(f'\nFound button "{button_name}" at {buttons}')
+                    logging.debug(f'Found button "{button_name}" at {buttons}')
                     button = buttons[0]
                     if len(buttons) > 1:
                         button = random.choice(buttons)
-                        print(f' > Randomly chosen button at {button}')
+                        logging.debug(f'Randomly chosen button at {button}')
                     x, y = button
                     pag.click(x, y)
-                    print(f' > Clicked it!')
+                    logging.debug(f'Clicked it!\n')
                     random_timeout()
             except Exception as e:
-                print(e)
                 pass
